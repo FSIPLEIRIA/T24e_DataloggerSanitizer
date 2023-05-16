8e857404d76446ad3b20b52e29eca27714fef253
carlostojal <carlos.tojal@hotmail.com>
2023-05-16 21:51:06 +0100

    documentation. read the configuration file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Datalogger Sanitizer
 
 Sanitize the CSV file to be more optimal to analyze data.
+
+## Configuration
+
+- ```n_bytes```: Number of bytes on each CAN frame
+- ```n_ids_max```: Number of IDs on the raw CSV - counting with blanks!!!
+- ```n_ids```: Number of used IDs
+- ```structure```: Structure of the CAN frames
+  - ```can_id```: CAN ID 
+  - ```vars```:
+    - ```name```: Textual description of the variable
+    - ```length```: Number of bytes allocated to this variable
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import sys
 import numpy as np
+import json
 
+# check for command line arguments
 if len(sys.argv) == 1:
     print("Insufficient arguments provided!")
     print(f"Usage: python {sys.argv[0]} [filename.csv]")
     sys.exit(-1)
 
-# load filename to numpy matrix
+# load configuration file
+print("Reading CAN configuration...", end='')
+f = open("can_config.json", "r")
+config = json.loads(f.read())
+f.close()
+print("Done!")
+
+# load csv file to numpy matrix
+print("Loading the data...", end='')
 mat = np.loadtxt(sys.argv[1], delimiter=';')
-print(mat)
+# print(mat)
+print("Done!")
+
+# slice the IDs based on the config
+ids_vector = mat[0:config['n_ids'],0]
+print(ids_vector)
+
+# iterate the matrix rows
+curr_row = 0
+while curr_row < mat.shape[0]:
+    curr_timestep = curr_row % config['n_ids_max']
+
+    curr_row += 1
